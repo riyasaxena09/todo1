d80016a0eb545bef8a848bf79a77a2beeb6f8013
riyasaxena09 <135497287+riyasaxena09@users.noreply.github.com>
2023-10-07 10:26:52 +0530

    add

---
+++ index.js
@@ -0,0 +1,11 @@
+import Navlink from "@/Component/navlink";
+import Form from "@/Component/form";
+function index(){
+  return (
+    <>
+    <Navlink></Navlink>
+    <Form></Form>
+    </>
+  )
+}
+export default index;
