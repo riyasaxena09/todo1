682cfc4b6f59948e7909cfe533fae15472a567e4
riyasaxena09 <135497287+riyasaxena09@users.noreply.github.com>
2023-10-07 10:26:31 +0530

    add

---
+++ navlink.js
@@ -0,0 +1,24 @@
+import styles from '../styles/navklink.module.css';
+function Navlink(){
+    return(
+<>
+<div>
+    <ul className={styles.list}>
+        <li className={styles.li1}>
+Todo
+        </li>
+        <li className={styles.li2}>
+about us
+</li>
+<li className={styles.li3}>
+contact us
+</li>
+<li className={styles.li3}>
+contact us
+</li>
+    </ul>
+</div>
+</>
+    )
+}
+export default Navlink;
